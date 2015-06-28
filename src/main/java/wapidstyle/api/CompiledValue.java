f543595f80bac5f78e86912520a04c39f9b7c003
wapidstyle <wapidstyle@live.ca>
2015-06-28 15:34:44 -0400

    Basically redo CompiledValue.java

--- src/main/java/wapidstyle/api/CompiledValue.java
+++ src/main/java/wapidstyle/api/CompiledValue.java
@@ -1,35 +1,23 @@
 package wapidstyle.api;
 
 public class CompiledValue {
-	public int integer;
-	public Integer integer2;
+	public Integer integer;
 	public String string;
+	public Boolean bool;
+	/** @deprecated */
 	public Boolean boolean1;
+	/** @deprecated */
 	public boolean boolean2;
+	/** @deprecated */
 	public static CompiledValue nulld = null;
+	public static CompiledValue nil = null;
 	
-	public static CompiledValue convertInt(Integer toConvert){
-		CompiledValue cv = new CompiledValue();
-		cv.integer = toConvert;
-		return cv;
-	}
-	public static CompiledValue convertInt(int toConvert){
-		return convertInt(new Integer(toConvert));
-	}
-	public static CompiledValue convertString(String toConvert){
-		CompiledValue cv = new CompiledValue();
-		cv.string = toConvert;
-		return cv;
-	}
-	public static CompiledValue convertString(string toConvert){
-		return convertString(toConvert.toString());
-	}
-	public static CompiledValue convertBoolean(boolean toConvert){
-		CompiledValue cv = new CompiledValue();
-		cv.boolean2 = toConvert;
-		return cv;
-	}
-	public static CompiledValue convertBoolean(Boolean toConvert){
-		return convertBoolean(toConvert.fresh);
+	public void set(Integer toAdd){integer = toAdd;}
+	public void set(int toAdd){set(new Integer(toAdd));}
+	public void set(String toAdd){string = toAdd;}
+	public void set(string toAdd){set(toAdd.toString());}
+	public void set(Boolean toAdd){bool = toAdd;}
+	public void set(java.lang.Boolean toAdd){set(new Boolean(toAdd.booleanValue()));}
+	public void set(boolean toAdd){set(new Boolean(toAdd));
 	}
 }
