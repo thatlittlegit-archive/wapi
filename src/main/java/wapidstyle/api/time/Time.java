8c055d0acc8ed7f7a77203e7e18f0e369f96551a
wapidstyle <wapidstyle@live.ca>
2015-06-29 17:18:47 -0400

    Add Time.java

---
+++ src/main/java/wapidstyle/api/time/Time.java
@@ -0,0 +1,19 @@
+package wapidstyle.api.time;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Time {
+	public long getNsTimeOfVM(){
+		return System.nanoTime();
+	}
+	public long getMsTimeFrom1970(){
+		return System.currentTimeMillis();
+	}
+	public String getCurrentTime(){
+		return LocalTime.now().toString();
+	}
+	public String getCurrentDate(){
+		return LocalDate.now().toString();
+	}
+}
