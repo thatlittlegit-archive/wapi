4ea3f6a101700de40e4498b691bdd8510d17f40b
wapidstyle <wapidstyle@live.ca>
2015-07-19 18:54:08 -0400

    Add VERY incomplete bukkit support :paperclip:

---
+++ src/main/java/wapidstyle/api/bukkit/Event.java
@@ -0,0 +1,7 @@
+package wapidstyle.api.bukkit;
+
+import org.bukkit.event.Listener;
+
+public class Event implements Listener{
+	public EventType type = EventType.UNDEFINED;
+}
---
+++ src/main/java/wapidstyle/api/bukkit/Plugin.java
@@ -0,0 +1,9 @@
+package wapidstyle.api.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	public void onEvent(Event e){
+		
+	}
+}
