bfae05a3f18aa4f37f639521a0def1beaa740432
wapidstyle <wapidstyle@live.ca>
2015-07-21 19:08:31 -0400

    [TEST] Change dir to pwd

--- travis/import_bukkit.sh
+++ travis/import_bukkit.sh
@@ -1,15 +1,15 @@
 echo Import_Bukkit Starting!
 cd ..
-dir
+pwd
 cd src
-dir
+pwd
 cd main
-dir
+pwd
 mkdir resources
 cd resources
-dir
+pwd
 wget https://hub.spigotmc.org/nexus/service/local/repositories/snapshots/content/org/bukkit/bukkit/1.8.7-R0.1-SNAPSHOT/bukkit-1.8.7-R0.1-20150718.232630-37.jar
 wget https://hub.spigotmc.org/nexus/service/local/repositories/central/content/org/avaje/ebeanorm/avaje-ebeanorm/4.8.1/avaje-ebeanorm-4.8.1.jar
-cd ..
-cd ..
-cd ..
+cd ~
+cd build
+cd wapidstyle
