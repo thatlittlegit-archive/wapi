55afc63364cc1459e0b581a206a42f51e1cb81ef
wapidstyle <wapidstyle@live.ca>
2015-07-21 19:00:25 -0400

    Make the resources directory rather than navigate to it

--- travis/import_bukkit.sh
+++ travis/import_bukkit.sh
@@ -2,7 +2,7 @@ echo Import_Bukkit Starting!
 cd ..
 cd src
 cd main
-cd resources
+mkdir resources
 wget https://hub.spigotmc.org/nexus/service/local/repositories/snapshots/content/org/bukkit/bukkit/1.8.7-R0.1-SNAPSHOT/bukkit-1.8.7-R0.1-20150718.232630-37.jar
 wget https://hub.spigotmc.org/nexus/service/local/repositories/central/content/org/avaje/ebeanorm/avaje-ebeanorm/4.8.1/avaje-ebeanorm-4.8.1.jar
 cd ..
