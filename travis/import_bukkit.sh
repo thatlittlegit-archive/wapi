786b96b244ea35d3ee072ae8486aeb4e4d40516b
wapidstyle <wapidstyle@live.ca>
2015-07-21 18:58:41 -0400

    Correct what I forgot in 60f3770f42

--- travis/import_bukkit.sh
+++ travis/import_bukkit.sh
@@ -5,4 +5,6 @@ cd main
 cd resources
 wget https://hub.spigotmc.org/nexus/service/local/repositories/snapshots/content/org/bukkit/bukkit/1.8.7-R0.1-SNAPSHOT/bukkit-1.8.7-R0.1-20150718.232630-37.jar
 wget https://hub.spigotmc.org/nexus/service/local/repositories/central/content/org/avaje/ebeanorm/avaje-ebeanorm/4.8.1/avaje-ebeanorm-4.8.1.jar
-cd ../../..
+cd ..
+cd ..
+cd ..
