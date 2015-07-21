34cba184b61a128c4d3e73d03212d18c0525bb52
wapidstyle <wapidstyle@live.ca>
2015-07-21 19:10:47 -0400

    [TEST] Continue adding pwds

--- travis/import_bukkit.sh
+++ travis/import_bukkit.sh
@@ -10,6 +10,9 @@ cd resources
 pwd
 wget https://hub.spigotmc.org/nexus/service/local/repositories/snapshots/content/org/bukkit/bukkit/1.8.7-R0.1-SNAPSHOT/bukkit-1.8.7-R0.1-20150718.232630-37.jar
 wget https://hub.spigotmc.org/nexus/service/local/repositories/central/content/org/avaje/ebeanorm/avaje-ebeanorm/4.8.1/avaje-ebeanorm-4.8.1.jar
-cd ~
-cd build
-cd wapidstyle
+cd ..
+pwd
+cd ..
+pwd
+cd ..
+pwd
