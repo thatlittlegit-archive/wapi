0ec70bb31133d3b9578f2c67ad3962d6268641e0
wapidstyle <wapidstyle@live.ca>
2015-07-21 18:54:58 -0400

    Fix import_bukkit.sh

--- travis/import_bukkit.sh
+++ travis/import_bukkit.sh
@@ -1,5 +1,5 @@
 echo Import_Bukkit Starting!
-cd src/main/resources
+cd ../src/main/resources
 wget https://hub.spigotmc.org/nexus/service/local/repositories/snapshots/content/org/bukkit/bukkit/1.8.7-R0.1-SNAPSHOT/bukkit-1.8.7-R0.1-20150718.232630-37.jar
 wget https://hub.spigotmc.org/nexus/service/local/repositories/central/content/org/avaje/ebeanorm/avaje-ebeanorm/4.8.1/avaje-ebeanorm-4.8.1.jar
 cd ../../..
