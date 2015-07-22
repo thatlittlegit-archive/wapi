5e240195dbad7c4b2289222ff627d4a86144263c
#wapidstyle <wapidstyle@live.ca>
2015-07-22 07:22:03 -0400

    Delete import_bukkit.sh

--- travis/import_bukkit.sh
+++
@@ -1,6 +0,0 @@
-echo Import_Bukkit Starting!
-wget https://hub.spigotmc.org/nexus/service/local/repositories/snapshots/content/org/spigotmc/spigot-api/1.8.7-R0.1-SNAPSHOT/spigot-api-1.8.7-R0.120150721.08211641.jar
-wget https://hub.spigotmc.org/nexus/service/local/repositories/central/content/org/avaje/ebeanorm/avaje-ebeanorm/4.8.1/avaje-ebeanorm-4.8.1.jar
-mv spigot-api-1.8.7-R0.120150721.08211641.jar spigot.jar
-mv avaje-ebeanorm-4.8.1.jar avaje.jar
-echo Import_Bukkit Done!
