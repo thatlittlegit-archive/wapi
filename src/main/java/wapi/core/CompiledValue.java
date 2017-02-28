66f8d6e61aa46b00676cf007a7ea63aef2d4a587
wapidstyle <wapidstyle@live.ca>
2017-02-28 14:18:23 +0000

    Add better constructors to CompiledValue

--- src/main/java/wapi/core/CompiledValue.java
+++ src/main/java/wapi/core/CompiledValue.java
@@ -14,12 +14,20 @@ public class CompiledValue {
 	public static CompiledValue nulld = null;
 	public static CompiledValue nil = null;
 	
-	public void set(Integer toAdd){integer = toAdd;}
-	public void set(int toAdd){set(new Integer(toAdd));}
-	public void set(String toAdd){string = toAdd;}
-	public void set(string toAdd){set(toAdd.toString());}
-	public void set(Boolean toAdd){bool = toAdd;}
-	public void set(java.lang.Boolean toAdd){set(new Boolean(toAdd.booleanValue()));}
-	public void set(boolean toAdd){set(new Boolean(toAdd));
+	public Integer set(Integer toAdd){integer = toAdd;return toAdd}
+	public int set(int toAdd){set(new Integer(toAdd));return toAdd}
+	public String set(String toAdd){string = toAdd;return toAdd}
+	public string set(string toAdd){set(toAdd.toString());return toAdd}
+	public Boolean set(Boolean toAdd){bool = toAdd;return toAdd}
+	public java.lang.Boolean set(java.lang.Boolean toAdd){set(new Boolean(toAdd.booleanValue()));return toAdd}
+	public boolean set(boolean toAdd){set(new Boolean(toAdd));return toAdd}
+	
+	public CompiledValue(boolean toSet){set(set);}
+	public CompiledValue(Boolean toSet){set(set);}
+	public CompiledValue(java.lang.Boolean toSet){set(set);}
+	public CompiledValue(String toSet){set(set);}
+	public CompiledValue(string toSet){set(set);}
+	public CompiledValue(Integer toSet){set(set);}
+	public CompiledValue(int toSet){set(set);}
 	}
 }
