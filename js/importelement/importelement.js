5b31f45268a6405ed42fde7268549c6aaac8764a
thatlittlegit <personal@thatlittlegit.tk>
2018-05-14 17:36:43 -0400

    Remove OOP from ImportElement and improve API

--- js/importelement/importelement.js
+++ js/importelement/importelement.js
@@ -1,57 +1,28 @@
-function ImportElement(){
-  var type = "javascript_error";
-  var src;
-  var async = true;
+function importResource(type, location, async, appendTo) {
+	switch(type){
+		case "css":
+			var elem = document.createElement('link');
+			elem.setAttribute("rel", "stylesheet");
+			elem.setAttribute("href", src);
 
-  var appendTo = "head";
+		break;
+		case "javascript":
+			var elem = document.createElement('script');
+			elem.setAttribute("type", "text/javascript");
+			elem.setAttribute("src", src);
+		break;
+		default:
+			throw new Error('No type specified!');
+	}
 
-  var ie;
+	if (async === true) {
+		elem.setAttribute("async", "true");
+	}
 
-  var construct = function(){
-    switch(type){
-      case "css":
-        this.type = "css";
-        this.ie = document.createElement('link');
-        this.ie.setAttribute("rel", "stylesheet");
-        if(async === true){this.ie.setAttribute("async", "true");}
-        this.ie.setAttribute("href", src);
-      break;
-      case "javascript" || "javascript_error":
-        this.ie = document.createElement('script');
-        this.ie.setAttribute("type", "text/javascript");
-        this.ie.setAttribute("src", src);
-        if(type === "javascript_error"){
-          console.warn("ImportElement: No type specified, using JavaScript!");
-        }
-      break;
-    }
-  };
-
-  var append = function(){
-    document.getElementsByTagName(appendTo)[0].appendChild(this.element);
-  };
+	document.querySelector(appendTo).append(elem);
 }
-function easyImport(type, src, appendTo){
-  var element = new ImportElement();
 
-  if(type !== undefined && src !== undefined){
-    switch(type){
-      case "js" || "javascript" || 0:
-        element.type = "javascript";
-        element.src = src;
-        if(appendTo !== undefined){
-          element.appendTo = appendTo;
-        }
-        element.construct();
-      break;
-      case "css" || "style" || "stylesheet" || 1:
-        element.type = "css";
-        element.src = src;
-        if(appendTo !== undefined){
-          element.appendTo = appendTo;
-        }
-        element.construct();
-      break;
-    }
-  }
+/* @deprecated */
+function easyImport(type, src, appendTo){
+	importResource(type, src, false, appendTo);
 }
