e79e135d9229a51c94e51718d477586fdd11b5fd
thatlittlegit <personal@thatlittlegit.tk>
2018-09-19 18:21:08 -0400

    Deprecate some Time functions

--- src/main/java/wapi/Time.java
+++ src/main/java/wapi/Time.java
@@ -4,12 +4,16 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Time {
+	/** @deprecated use System.nanoTime() */
 	public long getNsTimeOfVM(){
 		return System.nanoTime();
 	}
+
+	/** @deprecated use System.currentTimeMillis() **/
 	public long getMsTimeFrom1970(){
 		return System.currentTimeMillis();
 	}
+
 	public String getCurrentTime(){
 		return LocalTime.now().toString();
 	}
